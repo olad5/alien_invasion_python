82e79ba253578df6ba105de029edc69d7cc55d88
shonubifrancis <shonubioladipo@gmail.com>
2020-02-08 22:40:36 +0100

    imported ship module into main project and initialized the drawing of the ship on the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -15,8 +16,8 @@ class AlienInvasion:
             (self.settings.screen_width, self.settings.screen_height)
         )
         pygame.display.set_caption("Alien Invasion")
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """ Start the main loop for the game. """
@@ -26,7 +27,9 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             # Redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             # Make the most recently drawn screen visible
             pygame.display.flip()
 
