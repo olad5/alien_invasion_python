37b62d68efdd02f9a3ac3d2162d90ccb12a4a910
shonubifrancis <shonubioladipo@gmail.com>
2020-02-08 16:48:13 +0100

    created settings file and imported it into main project file

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -11,6 +12,8 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """ Start the main loop for the game. """
@@ -19,7 +22,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.bg_color)
             # Make the most recently drawn screen visible
             pygame.display.flip()
 
