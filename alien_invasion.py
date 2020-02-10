340105afd549b21a415855c948d16c61789e8661
shonubifrancis <shonubioladipo@gmail.com>
2020-02-10 18:25:25 +0100

    deleted old bullets, limited number of bullets and refactored bullet management code

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,9 +25,9 @@ class AlienInvasion:
         """ Start the main loop for the game. """
         while True:
             self._check_events()
-            self._update_screen()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
+            self._update_screen()
 
     def _check_events(self):
         """ Respond to keypresses and mouse events. """
@@ -60,8 +60,19 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """ Create a new bullet and add it to the bullets group. """
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """ Update position of bullets and get rid of old bullets. """
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """ Update images on the screen, amd flip to new screen """
