80a124a0bd199fd1bb0adab99d274f5779132d77
shonubifrancis <shonubioladipo@gmail.com>
2020-02-20 14:56:54 +0100

    added collision code and ssuccesfully completed the exercise :)

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,6 +81,17 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.right >= self.settings.screen_width:
                 self.bullets.remove(bullet)
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """ Respond to bullet-alien collisions. """
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         """ Update the postions of all aliens in the fleet. """
