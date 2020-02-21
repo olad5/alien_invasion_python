65e68f8001377dc24e42c7b4eebdef90bf748c53
shonubifrancis <shonubioladipo@gmail.com>
2020-02-21 10:40:29 +0100

    added play button

--- settings.py
+++ settings.py
@@ -21,6 +21,6 @@ class Settings:
 
         # Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 100
         # fleet directionof 1 represents right: -1 represents left
         self.fleet_direction = 1
