6e61cc4e745355038e33519239234a4a4666ecb3
shonubifrancis <shonubioladipo@gmail.com>
2020-03-01 18:19:20 +0100

    changed branch to reflect two exercises, done the first one, remaining the second one

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ class Settings:
         # Bullet settings
         # self.bullet_speed = 1.0
         self.bullet_width = 15
-        self.bullet_height = 30
+        self.bullet_height = 300
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
